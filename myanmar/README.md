9f2a312b22e9b26d6e79b4eede779eb6771b311a
andrewc <andrewc@SLV13060.(none)>
2013-04-30 13:04:24 +1000

    Myanmar Font Package 1.1, added Padauk Bold font

--- myanmar/README.md
+++ myanmar/README.md
@@ -2,17 +2,23 @@ Myanmar fonts package
 =====================
 
 myanmar\_fonts\_package-1.0-fx+an-android.xpi
+Version 1.1 (2013-04-30)
 
-Version 1.0 (2013-04-29)
+Released under the Mozilla Public License, version 2.0.
 
 Myanmar script fallback fonts for Firefox (Android edition) this addon 
 will install a set of Myanmar fonts that enables proper rendering of 
 Myanmar script on all sites (Facebook, Twitter, Google, etc.), 
 regardless of whether a font has been embedded into the web page.
 
+Acknowledgements
+----------------
+
+These addons were based on Mathieu Pellerin's Khmer Fonts Package and developed with his advice 
+and support: https://addons.mozilla.org/en-us/android/addon/khmer-fonts-package/
+
 The Padauk font has been included. Padauk was developed by Debbi and 
-Martin Hosken, andwas released under the SIL Open Font License 1.1.
+Martin Hosken, and was released under the SIL Open Font License 1.1.
 
 Font and font source files are available at http://scripts.sil.org/Padauk
-
 
--- myanmar/bootstrap.js
+++ myanmar/bootstrap.js
@@ -1,5 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 Components.utils.import("resource://gre/modules/Services.jsm");
-
+    
 var Cc = Components.classes;
 var Ci = Components.interfaces;
 
@@ -21,34 +24,50 @@ function startup(data, reason) {
    fontdir.permissions = 0777;
   }
   
+  var fromDir = null;
   var fromFile = null;
   var checkFile = null;
   var fontAdded = false;
 
-  function linkFromFile() {
-   fromFile = Cc["@mozilla.org/file/directory_service;1"].
+  function linkFromDir() {
+   fromDir = Cc["@mozilla.org/file/directory_service;1"].
                   getService(Ci.nsIProperties).
                   get("ProfD", Ci.nsIFile);
-   fromFile.append("extensions");
-   fromFile.append("myanmarfonts@openroad.net.au");
-   fromFile.append("defaults");
-   fromFile.append("Padauk.ttf");
+   fromDir.append("extensions");
+   fromDir.append("myanmarfonts@openroad.net.au");
+   fromDir.append("defaults");
   }
   
   //when v19 moves to beta, remove font check in root user profile dir
   checkFile = profiledir.clone();
   checkFile.append("Padauk.ttf");
-  if (!checkFile.exists()) { 
-   if (fromFile == null) linkFromFile();
+  if (!checkFile.exists()) {
+   if (fromDir == null) linkFromDir();
+   
+   fromFile = fromDir.clone();
+   fromFile.append("Padauk.ttf");
    fromFile.copyTo(profiledir,"Padauk.ttf");
+
+   fromFile = fromDir.clone();
+   fromFile.append("Padauk-bold.ttf");
+   fromFile.copyTo(profiledir,"Padauk-bold.ttf");
+
    fontAdded = true;
   }
   
   checkFile = fontdir.clone();
   checkFile.append("Padauk.ttf");
   if (!checkFile.exists()) { 
-   if (fromFile == null) linkFromFile();
+   if (fromDir == null) linkFromDir();
+   
+   fromFile = fromDir.clone();
+   fromFile.append("Padauk.ttf");
    fromFile.copyTo(fontdir,"Padauk.ttf");
+   
+   fromFile = fromDir.clone();
+   fromFile.append("Padauk-bold.ttf");
+   fromFile.copyTo(fontdir,"Padauk-bold.ttf");
+   
    fontAdded = true;
   } 
   
@@ -62,24 +81,29 @@ function startup(data, reason) {
 function shutdown(data, reason) {
  if (reason == 4) {
   //be nice, clean up
+  var checkFile = null;
   var profiledir = Cc["@mozilla.org/file/directory_service;1"].
                   getService(Ci.nsIProperties).
                   get("ProfD", Ci.nsIFile);
   var fontdir = profiledir.clone();
   fontdir.append("fonts");
 
-  var checkFile = profiledir.clone();
+  checkFile = profiledir.clone();
   checkFile.append("Padauk.ttf");
-  if (checkFile.exists()) {
-   checkFile.remove(true);
-  }
+  if (checkFile.exists()) checkFile.remove(true);
+  
+  checkFile = profiledir.clone();
+  checkFile.append("Padauk-bold.ttf");
+  if (checkFile.exists()) checkFile.remove(true);
  
   if (fontdir.exists()) {
    checkFile = fontdir.clone();
    checkFile.append("Padauk.ttf");
-   if (checkFile.exists()) {
-    checkFile.remove(true);
-   }
+   if (checkFile.exists()) checkFile.remove(true);
+
+   checkFile = fontdir.clone();
+   checkFile.append("Padauk-bold.ttf");
+   if (checkFile.exists()) checkFile.remove(true);
   }
 
   var fonts = Cc["@mozilla.org/gfx/fontenumerator;1"].getService(Components.interfaces.nsIFontEnumerator);
